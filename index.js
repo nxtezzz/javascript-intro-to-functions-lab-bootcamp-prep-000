28a98801c2f4ff518d273eb33928e4d6668189ef
Nathaniel Woodbury <nxtezzz@gmail.com>
2017-10-11 01:43:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function shout(string) {
     return string.toUpperCase() }
 
+function whisper(string) {
+    return string.toLowerCase() }
+
+function logShout(string) {
+    console.log(string.toUpperCase()) }
+
 function sayHiToGrandma(string) {
-  return string.toLowerCase() }
+    if (string.toUpperCase() === string) {
+      return "Yes Indeed" }
+
+    else if (string === "I love you, grandma") {
+      return "I love you, too" }
+      else {
+        return ("I CANT HEAR YOU")
+      }
