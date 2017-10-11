79b55d0a021f500748a291585d92582daecc2a00
Nathaniel Woodbury <nxtezzz@gmail.com>
2017-10-11 01:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function shout(string) {
     return string.toUpperCase() }
 
-function sayHiToGrandma(string)
-  return string "I can\'t hear you!" if `string` is lowercase'
+function sayHiToGrandma(string) {
+  return string () }
